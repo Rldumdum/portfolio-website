a4c08c393f4e9292a1f7af471a6cd26500e29311
Rldumdum <juelierldumdum@gmail.com>
2024-01-14 19:32:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button>
+        <p className="font-mono text-5xl">Check my work</p>
+      </button>
+    </div>
+  );
+}
+
+export default App;
